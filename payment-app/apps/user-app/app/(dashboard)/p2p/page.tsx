be001d68025db025a9da804d08e84fd833d21cc2
Nikhil <ace.nikchauhan@gmail.com>
2025-02-05 19:50:29 +0530

    payment-project: added p2p transaction

---
+++ payment-app/apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,11 @@
+import P2pTransfer from "../../../components/p2pTransfer";
+
+const P2p = () => {
+  return (
+    <div className="w-full">
+      <P2pTransfer />
+    </div>
+  );
+};
+
+export default P2p;
